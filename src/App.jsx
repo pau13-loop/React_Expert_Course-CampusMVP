b27cd5c2f7566dc4103ffc52287e7d44454fdd62
Pau Llinas Amat <pllinas@intricom.es>
2023-10-11 12:31:36 +0200

    refactor: :truck: Move resources Lesson 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,84 +1,13 @@
 import { createElement } from 'react'
 import './App.css'
-
-// ### Ejemplo con   --> se visualiza como código JSX transpilado por BABEL ###
-
-const FormularioBusqueda = () => {
-  return createElement(
-    "form",
-    { action: "/search", emthod: "GET" },
-    createElement(
-      "input",
-      { type: "search", placeholder: "Términos de búsqueda" }
-    ),
-    createElement("button", { type: "submit" }, "Buscar")
-  );
-}
-
-const Cabecera = () => {
-  return createElement(
-    "div",
-    null,
-    createElement("h1", null, "Mi primera app React"),
-    createElement(FormularioBusqueda)
-  );
-}
-
-const Desplegable = (props) => {
-  return createElement(
-    "details",
-    null,
-    createElement("summary", null, props.titulo),
-    createElement("p", null, props.contenido)
-  );
-}
+import SyntaxCompare from './lesson_1/old-syntax-vs-new-syntax';
 
 function App() {
-  return createElement(
-    "div",
-    { className: "App" },
-    createElement(Cabecera),
-    createElement(Desplegable, { 
-      titulo: "Título 1", 
-      contenido: "Contenido 1" 
-    }),
-    createElement(Desplegable, {
-      titulo: "Título 2",
-      contenido: "Contenido 2"
-    })
+  return (
+    <>
+      <SyntaxCompare />
+    </>
   );
 }
-
-// ### Ejemplo con sintaxis JSX ###
-/*
-const FormularioBusqueda = () => {
-  return <form action='search' method='GET'>
-    <input type="search" placeholder="Términos de búsqueda" />
-    <button type="submit">Buscar</button>
-  </form>
-}
-
-const Cabecera = () => {
-  return <div>
-    <h1>Mi primera app React</h1>
-    <FormularioBusqueda />
-  </div>
-}
-
-const Desplegable = (props) => {
-  return <details>
-    <summary>{props.titulo}</summary>
-    <p>{props.contenido}</p>
-  </details>
-}
-
-function App() {
-  return <div className='App'>
-    <Cabecera />
-    <Desplegable titulo="Título 1" contenido="Contenido 1" />
-    <Desplegable titulo="Título 2" contenido="Contenido 2" />
-  </div>
-}
-*/
 
 export default App
---
+++ src/lesson_1/old-syntax-vs-new-syntax.jsx
@@ -0,0 +1,81 @@
+import { createElement } from 'react'
+
+// ### Ejemplo con   --> se visualiza como código JSX transpilado por BABEL ###
+
+const FormularioBusqueda = () => {
+  return createElement(
+    "form",
+    { action: "/search", emthod: "GET" },
+    createElement(
+      "input",
+      { type: "search", placeholder: "Términos de búsqueda" }
+    ),
+    createElement("button", { type: "submit" }, "Buscar")
+  );
+}
+
+const Cabecera = () => {
+  return createElement(
+    "div",
+    null,
+    createElement("h1", null, "Mi primera app React"),
+    createElement(FormularioBusqueda)
+  );
+}
+
+const Desplegable = (props) => {
+  return createElement(
+    "details",
+    null,
+    createElement("summary", null, props.titulo),
+    createElement("p", null, props.contenido)
+  );
+}
+
+export default function SyntaxCompare() {
+    return createElement(
+      "div",
+      { className: "App" },
+      createElement(Cabecera),
+      createElement(Desplegable, { 
+        titulo: "Título 1", 
+        contenido: "Contenido 1" 
+      }),
+      createElement(Desplegable, {
+        titulo: "Título 2",
+        contenido: "Contenido 2"
+      })
+    );
+}
+
+// ### Ejemplo con sintaxis JSX ###
+/*
+const FormularioBusqueda = () => {
+  return <form action='search' method='GET'>
+    <input type="search" placeholder="Términos de búsqueda" />
+    <button type="submit">Buscar</button>
+  </form>
+}
+
+const Cabecera = () => {
+  return <div>
+    <h1>Mi primera app React</h1>
+    <FormularioBusqueda />
+  </div>
+}
+
+const Desplegable = (props) => {
+  return <details>
+    <summary>{props.titulo}</summary>
+    <p>{props.contenido}</p>
+  </details>
+}
+
+export default function SyntaxCompare() {
+    return <div className='App'>
+    <Cabecera />
+    <Desplegable titulo="Título 1" contenido="Contenido 1" />
+    <Desplegable titulo="Título 2" contenido="Contenido 2" />
+  </div>
+}
+*/
